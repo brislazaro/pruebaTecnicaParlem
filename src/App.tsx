fb862a38b4c01a3d65d6ba34ea5c56b45d7e2781
brislazaro <brisa.lazaroc@gmail.com>
2024-05-31 12:38:04 +0200

    fix tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="flex justify-center items-center p-6">
+      <p className="">tailwind works</p>
+    </div>
+  );
 }
 
 export default App;
