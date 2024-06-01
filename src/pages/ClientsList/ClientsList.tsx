3b677698ca874635dc61c3cfd3bccbe32f766392
brislazaro <brisa.lazaroc@gmail.com>
2024-06-01 17:28:49 +0200

    improve clients list error and loading states

--- src/pages/ClientsList/ClientsList.tsx
+++ src/pages/ClientsList/ClientsList.tsx
@@ -4,25 +4,27 @@ import useClientsList from "./useClientsList";
 const ClientsList = () => {
   const { isLoading, isError, data } = useClientsList();
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error</p>;
-  }
-
   return (
     <div className="w-full p-4 border  rounded-lg shadow md:p-8 bg-darkgray border-gray-700">
       <h1 className="text-xl font-bold leading-none text-white mb-4">
         Clientes
       </h1>
 
-      <ul className="divide-y divide-gray-700 ">
-        {data.map((customer) => (
-          <ClientCard customer={customer} key={customer._id} />
-        ))}
-      </ul>
+      {isLoading && !isError && <p>Loading...</p>}
+
+      {!isLoading && isError && (
+        <div className="p-4 bg-yellow text-lg rounded-md">
+          ❌ Error al cargar los clientes
+        </div>
+      )}
+
+      {!isLoading && !isError && (
+        <ul className="divide-y divide-gray-700 ">
+          {data.map((customer) => (
+            <ClientCard customer={customer} key={customer._id} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import { MemoryRouter } from "react-router-dom";
 
 export const renderComponentFactory = (component: ReactNode) => {
