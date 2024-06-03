93d75c9d7e1fe652bcba5257b673f9b70078dc5e
brislazaro <brisa.lazaroc@gmail.com>
2024-06-03 19:22:27 +0200

    Add API_URL constant

--- src/api/getClients.ts
+++ src/api/getClients.ts
@@ -1,7 +1,8 @@
+import { API_URL } from "../constants";
 import { Customer } from "../types";
 
 export const getClients = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/clients`);
+  const response = await fetch(`${API_URL}/clients`);
   const apiData: Customer[] = await response.json();
 
   return apiData;
--- src/api/getProducts.ts
+++ src/api/getProducts.ts
@@ -1,7 +1,8 @@
+import { API_URL } from "../constants";
 import { Product } from "../types";
 
 export const getProducts = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/products`);
+  const response = await fetch(`${API_URL}/products`);
   const apiData: Product[] = await response.json();
 
   return apiData;
